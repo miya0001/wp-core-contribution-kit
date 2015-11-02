214c937ac3f68476e482415a4fd2498caf91313f
miya0001 <miya@wpist.me>
2015-11-03 01:32:28 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,79 @@
+# WordPress Installer on your desktop.
+
+WordPress development environment with PHP built-in web server + WP-CLI.
+
+## Requires
+
+* OSX
+* php 5.4 or later
+* MySQL
+
+### Recommend
+
+* [Composer](https://getcomposer.org/)
+* [Mailcatcher](http://mailcatcher.me/)
+
+## Uage
+
+```
+$ curl https://.../run.sh | bash -s <db-user> <db-pass> <db-name>
+```
+
+or
+
+```
+$ ./run.sh <db-user> <db-pass> <db-name>
+```
+
+### Defaults
+
+* db-user: `root`
+* db-pass: (empty)
+* db-name: `wpdev`
+
+## How to use
+
+```
+$ mkdir ~/Desktop/wordpress && cd $_
+$ curl https://raw.githubusercontent.com/miya0001/wp-instant-setup/master/run.sh | bash
+```
+
+Or
+
+```
+$ git clone git@github.com:miya0001/wp-instant-setup.git && cd wp-instant-setup
+$ ./run.sh
+```
+
+### For MAMP users
+
+```
+$ mkdir ~/Desktop/wordpress && cd $_
+$ curl https://raw.githubusercontent.com/miya0001/wp-instant-setup/master/run.sh | bash -s root root
+```
+
+Or
+
+```
+$ git clone git@github.com:miya0001/wp-instant-setup.git && cd wp-instant-setup
+$ ./run.sh root root
+```
+
+## Default Account
+
+* User: `admin`
+* Password: `admin`
+
+## Advanced Tips
+
+Add alias into your `~/.bash_profile` like following.
+
+```
+alias wpserve="curl https://raw.githubusercontent.com/miya0001/wp-instant-setup/master/run.sh | bash -s <db-user> <db-pass>"
+```
+
+Then just run:
+
+```
+$ wpserve <db-name>
+```
---
+++ run.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+set -ex;
+
+mysql.server start
+
+DB_USER=${1-root}
+DB_PASS=$2
+DB_NAME=${3-wpdev}
+PORT=8080
+WP_PATH=$(pwd)/trunk/src
+WP_TITLE='Welcome to the WordPress'
+WP_DESC='Hello World!'
+
+if [ -e "$WP_PATH/wp-config.php" ]; then
+    open http://127.0.0.1:$PORT
+    wp server --host=0.0.0.0 --port=$PORT --docroot=$WP_PATH
+    exit 0
+fi
+
+echo "path: www" > $(pwd)/wp-cli.yml
+
+if [ $DB_PASS ]; then
+    echo "DROP DATABASE IF EXISTS $DB_NAME;" | mysql -u$DB_USER -p$DB_PASS
+    echo "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql -u$DB_USER -p$DB_PASS
+else
+    echo "DROP DATABASE IF EXISTS $DB_NAME;" | mysql -u$DB_USER
+    echo "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql -u$DB_USER
+fi
+
+svn co http://develop.svn.wordpress.org/trunk/ trunk
+
+if [ $DB_PASS ]; then
+wp core config \
+--dbhost=localhost \
+--dbname="$DB_NAME" \
+--dbuser="$DB_USER" \
+--dbpass="$DB_PASS" \
+--dbprefix=wp_ \
+--locale=en_US \
+--extra-php <<PHP
+define( 'JETPACK_DEV_DEBUG', true );
+define( 'WP_DEBUG', true );
+PHP
+else
+wp core config \
+--dbhost=localhost \
+--dbname=$DB_NAME \
+--dbuser=$DB_USER \
+--dbprefix=wp_ \
+--locale=en_US \
+--extra-php <<PHP
+define( 'JETPACK_DEV_DEBUG', true );
+define( 'WP_DEBUG', true );
+PHP
+fi
+
+wp core install \
+--url=http://127.0.0.1:$PORT \
+--title="WordPress" \
+--admin_user="admin" \
+--admin_password="admin" \
+--admin_email="admin@example.com"
+
+bin/wp rewrite structure "/archives/%post_id%"
+
+bin/wp option update blogname "$WP_TITLE"
+bin/wp option update blogdescription "$WP_DESC"
+
+open http://127.0.0.1:$PORT
+wp server --host=0.0.0.0 --port=$PORT --docroot=$WP_PATH

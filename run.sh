b9446bf48d63ceedb8ff6323174a5262f327816a
miya0001 <miya@wpist.me>
2015-12-06 11:08:06 -0500

    change path to wp

--- run.sh
+++ run.sh
@@ -50,7 +50,7 @@ cat trunk/wp-tests-config-sample.php \
 | sed -e s/yourpasswordhere/$DB_PASS/ > trunk/wp-tests-config.php
 
 if [ $DB_PASS ]; then
-wp core config \
+bin/wp core config \
 --dbhost=localhost \
 --dbname="$DB_NAME" \
 --dbuser="$DB_USER" \
@@ -62,7 +62,7 @@ define( 'JETPACK_DEV_DEBUG', true );
 define( 'WP_DEBUG', true );
 PHP
 else
-wp core config \
+bin/wp core config \
 --dbhost=localhost \
 --dbname=$DB_NAME \
 --dbuser=$DB_USER \
@@ -74,17 +74,17 @@ define( 'WP_DEBUG', true );
 PHP
 fi
 
-wp core install \
+bin/wp core install \
 --url=http://127.0.0.1:$PORT \
 --title="WordPress" \
 --admin_user="admin" \
 --admin_password="admin" \
 --admin_email="admin@example.com"
 
-wp rewrite structure "/archives/%post_id%"
+bin/wp rewrite structure "/archives/%post_id%"
 
-wp option update blogname "$WP_TITLE"
-wp option update blogdescription "$WP_DESC"
+bin/wp option update blogname "$WP_TITLE"
+bin/wp option update blogdescription "$WP_DESC"
 
 open http://127.0.0.1:$PORT
-wp server --host=0.0.0.0 --port=$PORT --docroot=$WP_PATH
+bin/wp server --host=0.0.0.0 --port=$PORT --docroot=$WP_PATH

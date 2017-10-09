fd8859d75deb52fa6125f2eebd50ed537652c3d7
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2017-10-09 13:03:59 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -17,13 +17,6 @@ if [ -e "$WP_PATH/wp-config.php" ]; then
     exit 0
 fi
 
-if ! bin/wp --info ; then
-    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli-nightly.phar
-    rm -fr bin && mkdir bin
-    mv wp-cli-nightly.phar bin/wp
-    chmod 755 bin/wp
-fi
-
 echo "path: trunk/src" > $(pwd)/wp-cli.yml
 
 if [ $DB_PASS ]; then
@@ -48,7 +41,7 @@ cat trunk/wp-tests-config-sample.php \
 | sed -e s/yourpasswordhere/$DB_PASS/ > trunk/wp-tests-config.php
 
 if [ $DB_PASS ]; then
-bin/wp core config \
+wp core config \
 --dbhost=localhost \
 --dbname="$DB_NAME" \
 --dbuser="$DB_USER" \
@@ -60,7 +53,7 @@ define( 'JETPACK_DEV_DEBUG', true );
 define( 'WP_DEBUG', true );
 PHP
 else
-bin/wp core config \
+wp core config \
 --dbhost=localhost \
 --dbname=$DB_NAME \
 --dbuser=$DB_USER \
@@ -72,17 +65,17 @@ define( 'WP_DEBUG', true );
 PHP
 fi
 
-bin/wp core install \
+wp core install \
 --url=http://127.0.0.1:$PORT \
 --title="WordPress" \
 --admin_user="admin" \
 --admin_password="admin" \
 --admin_email="admin@example.com"
 
-bin/wp rewrite structure "/archives/%post_id%"
+wp rewrite structure "/archives/%post_id%"
 
-bin/wp option update blogname "$WP_TITLE"
-bin/wp option update blogdescription "$WP_DESC"
+wp option update blogname "$WP_TITLE"
+wp option update blogdescription "$WP_DESC"
 
 open http://127.0.0.1:$PORT
-bin/wp server --host=0.0.0.0 --port=$PORT --docroot=$WP_PATH
+wp server --host=0.0.0.0 --port=$PORT --docroot=$WP_PATH
